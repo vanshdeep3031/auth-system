2a6c686f3216b3d05d6a270677d8b24119c38dc5
vanshdeep3031 <vansh.0112deep@gmail.com>
2026-04-08 11:50:30 +0530

    added root route

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
+import cors from "cors";
 
+app.use(cors());
 dotenv.config();
 
 const app = express();
